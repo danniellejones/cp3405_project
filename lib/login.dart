07f5a6ff880810c921776f8d62813bf285d54402
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-09-18 13:03:56 +1000

    Updated code

--- lib/login.dart
+++ lib/login.dart
@@ -11,34 +11,59 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
+Widget buildUsername() {
+  return const Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: TextField(
+          decoration: InputDecoration(
+              border: OutlineInputBorder(), hintText: 'Username'),
+          textAlign: TextAlign.center,
+          keyboardType: TextInputType.name, // Not sure if name is correct
+        ),
+      )
+    ],
+  );
+}
+
+Widget buildPassword() {
+  return const Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(), hintText: 'Enter password'),
+          textAlign: TextAlign.center,
+          keyboardType: TextInputType.name, // Not sure if name is correct
+        ),
+      )
+    ],
+  );
+}
+
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: const Text('Login Screen')),
-      body: const Column(
+      appBar: AppBar(title: const Text('Welcome')),
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             'Login',
-            style: TextStyle(fontSize: 40),
+            style: TextStyle(
+                color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Username'),
-                textAlign: TextAlign.center),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Password'),
-                textAlign: TextAlign.center),
-          ),
+          const SizedBox(height: 50),
+          buildUsername(),
+          buildPassword()
         ],
       ),
     ));
