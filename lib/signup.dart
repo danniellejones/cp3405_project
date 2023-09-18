1b633c7fe819eada052f38c2ccda3cdbcc3ee566
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-09-18 17:13:53 +1000

    Created sign up page

---
+++ lib/signup.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('World of Schoolcraft'),
+      ),
+    );
+  }
+}
