8cf494ddb86af76ac44b94f4088c939f95122672
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-09-23 13:33:44 +1000

    Added submit button to UI

--- lib/reset_password.dart
+++ lib/reset_password.dart
@@ -1,6 +1,54 @@
 import 'package:flutter/material.dart';
 
 class ResetPasswordPage extends StatelessWidget {
+  Widget buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            prefixIcon: Icon(Icons.lock),
+            hintText: 'New Password',
+          ),
+          textAlign: TextAlign.left,
+          keyboardType: TextInputType.text,
+        ),
+        SizedBox(height: 10),
+        TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.lock),
+              hintText: 'Confirm password',
+            ),
+            textAlign: TextAlign.left,
+            keyboardType: TextInputType.text)
+      ],
+    );
+  }
+
+  Widget buildSubmitBtn() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 25),
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            elevation: 5,
+            minimumSize: const Size.fromHeight(50)),
+        child: const Text(
+          'Submit',
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +66,10 @@ class ResetPasswordPage extends StatelessWidget {
                   fontSize: 40,
                   fontWeight: FontWeight.bold),
             ),
+            SizedBox(height: 50),
+            buildPassword(),
+            SizedBox(height: 20),
+            buildSubmitBtn()
           ],
         ),
       ),
