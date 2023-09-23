4a8242f0247efdbaff2d46ea106c983bc8406a88
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-09-24 01:04:02 +1000

    Updating UI Recovery code page

--- lib/recovery_code.dart
+++ lib/recovery_code.dart
@@ -1,5 +1,6 @@
 import 'package:cp3405_project/reset_password.dart';
 import 'package:flutter/material.dart';
+import 'package:cp3405_project/responsive_layout.dart';
 import 'package:flutter_verification_code/flutter_verification_code.dart';
 
 class RecoveryCodePage extends StatefulWidget {
@@ -85,33 +86,71 @@ class _RecoveryCodePageState extends State<RecoveryCodePage> {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('World of Schoolcraft')),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: SizedBox(
+      height: height,
+      width: width,
+      child: SingleChildScrollView(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const Text(
-              'Verification',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 50),
-            const Text(
-                'We sent you a recovery code, enter it below.\n\nRecovery code was sent to your_email@example.com'),
-            buildRecoveryCode(),
-            const SizedBox(height: 20),
-            buildResendCode(),
-            const SizedBox(height: 20),
-            buildVerifyBtn()
+            ResponsiveLayout.isMobileScreen(context)
+                ? const SizedBox()
+                : Expanded(
+                    // Left side of screen
+                    child: Container(
+                      height: height,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                'lib/assets/images/img_classroom.png'),
+                            fit: BoxFit.fill),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'World of Schoolcraft',
+                          style: TextStyle(
+                              fontSize: 48.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+
+            // Right side of screen
+            Expanded(
+                child: Container(
+                    padding: EdgeInsets.all(20),
+                    height: height,
+                    color: Colors.white,
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(height: height * 0.2),
+                          Text(
+                            'Verification',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 34.0),
+                          ),
+                          SizedBox(height: 30),
+                          Text(
+                              'We sent you a recovery code, enter it below.\nRecovery code was sent to your_email@example.c',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.grey)),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                    )))
           ],
         ),
       ),
-    );
+    ));
   }
 }
